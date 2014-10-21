ac442e67af35af4190464b4bf5f11ff7c4a5ff3c
industrial-sloth <industrial-sloth@users.noreply.github.com>
2014-10-21 16:39:03 -0400

    swap image height and width when reading tifs in seriesloader

--- python/thunder/rdds/fileio/seriesloader.py
+++ python/thunder/rdds/fileio/seriesloader.py
@@ -280,7 +280,10 @@ class SeriesLoader(object):
             pass
 
         # get dimensions
-        dims = (firstifd.getImageWidth(), firstifd.getImageHeight(), len(tiffheaders.ifds))
+        # dims = (firstifd.getImageWidth(), firstifd.getImageHeight(), len(tiffheaders.ifds))
+        # BEGIN test ---
+        dims = (firstifd.getImageHeight(), firstifd.getImageWidth(), len(tiffheaders.ifds))
+        # END test ---
 
         # get datatype
         bitspersample = firstifd.getBitsPerSample()
